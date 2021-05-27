514e62b5b1077118f0229950a1dd3dfe06b03fda
Flavio <flavio.origgi@outlook.it>
2021-05-27 14:47:13 +0200

    fatto un po' di css

--- js/script.js
+++ js/script.js
@@ -13,13 +13,26 @@ console.log(anni);
 
 // il prezzo del biglietto, 0.21 € al km
 var prezzoAlKm = (distanza * 0.21).toFixed(2);
-console.log('costo fascia standard', prezzoAlKm);
+console.log('costo fascia standard €:', prezzoAlKm);
 
 // variabilie eta <18 (https://qastack.it/programming/3163070/javascript-displaying-a-float-to-2-decimal-places)
 if(anni < 18){
-    console.log('costo fascia minorenne', (prezzoAlKm - (prezzoAlKm / 100 * 20)).toFixed(2));
+    var minorenne = (prezzoAlKm - (prezzoAlKm / 100 * 20)).toFixed(2);
+    console.log('costo fascia minorenne €:', (prezzoAlKm - (prezzoAlKm / 100 * 20)).toFixed(2));
 } else if(anni > 65) {
-    console.log('costo fascia anziano', (prezzoAlKm - (prezzoAlKm / 100 * 40)).toFixed(2));
+    var anziano = (prezzoAlKm - (prezzoAlKm / 100 * 40)).toFixed(2);
+    console.log('costo fascia anziano €:', (prezzoAlKm - (prezzoAlKm / 100 * 40)).toFixed(2));
 }
+
+
+// a video
+document.getElementById('costo-standard').innerHTML = prezzoAlKm;
+
+document.getElementById('costo-minorenne').innerHTML = minorenne;
+
+document.getElementById('costo-anziano').innerHTML = anziano;
+
+
+
 
 
